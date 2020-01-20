54a1bebc201b4ae67b641dbd01bd9535f9b47d18
zhanghua <zhanghua@elex.com>
2020-01-20 15:47:24 +0800

    使用离屏的canvas实现了首屏的渲染,可以最少化代码量

--- wechatgame/game.js
+++ wechatgame/game.js
@@ -1,3 +1,7 @@
+'use strict';
+
+let offScreenCanvas = require('./offScreenCanvas');
+
 const { windowWidth, windowHeight } = wx.getSystemInfoSync();
 wx.setPreferredFramesPerSecond(10); // 10足够了,不需要更高的帧率,因为开始时我们主要是在加载资源.
 
@@ -16,42 +20,16 @@ _main_();
 function _main_() {
   let errLog = "";  // 用来存储错误日志
 
-  // 先加载首屏背景纹理,因为它是异步加载的,加载过程中可以同步做后面的初始化工作
-  const bgImageLoading = loadImage("first_package_images/first_flash.jpg");
-  
-  // 显示首屏背景纹理的 Vertex Shader
-  const vsSource = `
-    attribute vec2 aVertexPosition;
-    attribute vec2 aTextureCoord;
-
-    varying highp vec2 vTextureCoord;
-
-    void main(void){
-      gl_Position = vec4(aVertexPosition, 0.0, 1.0);
-      vTextureCoord = aTextureCoord;
-  }
-  `;
-
-  // 显示首屏纹理的 Fragment Shader
-  const fsSource = `
-    varying highp vec2 vTextureCoord;
-
-    uniform sampler2D uSampler;
-
-    void main(void){
-      gl_FragColor = texture2D(uSampler, vTextureCoord);
-      // gl_FragColor = vec4(0, 0.8, 0, 1);
-    }
-  `;
-
   const canvas = wx.createCanvas();   // 创建画布
-  screencanvas = initCanvas(canvas);              // 强制 cocos 引擎使用同一个 canvas
-  var gl = canvas.getContext('webgl');// 获取 webgl
+  GameGlobal.screencanvas = initCanvas(canvas);  // 强制 cocos 引擎使用同一个 canvas,见 stage1\libs\weapp-adapter\HTMLCanvasElement.js:26 处的实现
+  let gl = canvas.getContext('webgl');// 获取 webgl
 
-  // Initialize a shader program; this is where all the lighting
-  // for the vertices and so forth is established.
-  const shaderProgram = initShaderProgram(gl, vsSource, fsSource);
-  if(shaderProgram == null){
+  // 初始化真正的离屏 canvas,可以简少代码量,用来渲染首屏. 注: 尽量早的执行此函数,因为可以早点加载需要的资源
+  const offScreenPromise = offScreenCanvas.init(gl);
+
+  // 初始化离屏渲染使用的 shader
+  const shaderProgram = initOffScreenShaderProgram(gl);
+  if (shaderProgram == null) {
     wx.showModal({
       title: '初始化首屏 Shader 失败',
       content: errLog,
@@ -65,11 +43,11 @@ function _main_() {
   // look up uniform locations.
   let programInfo = {
     program: shaderProgram,
-    attribLocations:{
+    attribLocations: {
       vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
       textureCoord: gl.getAttribLocation(shaderProgram, 'aTextureCoord'),
     },
-    uniformLocations:{
+    uniformLocations: {
       uSampler: gl.getUniformLocation(shaderProgram, 'uSampler'),
     },
   };
@@ -77,63 +55,61 @@ function _main_() {
   // Here's where we call the routine that builds all the 
   // objects we'll be drawing
   let buffers;
-
-  let firstFlashTexture = null;
+  let fullScreenTexture = null; // 全屏的纹理
 
   // 首屏图片加载完成后初始化 webgl 纹理
-  bgImageLoading.then(function(image){
+  offScreenPromise.then(function (texture) {
     // 加载首屏纹理
-    firstFlashTexture = loadTexture(gl, image);
-    buffers = initQuadBuffers(gl, image.width, image.height);
-    image.src = ""; // 清除图片的内存占用
+    fullScreenTexture = texture;
+    buffers = initFullScreenQuadBuffers(gl);
   });
 
   // 主循环函数,显示loading条
-  function loop(){
-    if(programInfo === null){
-      return ; // 已经加载完成子包,不再需要此首屏渲染了
+  function loop() {
+    if (programInfo === null) {
+      return; // 已经加载完成子包,不再需要此首屏渲染了
     }
-    
-    if(firstFlashTexture){
-      drawScene(gl, programInfo, buffers, firstFlashTexture, 0);
-    }    
+
+    if (fullScreenTexture) {
+      drawScene(gl, programInfo, buffers, fullScreenTexture, 0);
+    }
     requestAnimationFrame(loop);
   }
 
-  if(true){
-  // 加载子包
-  const loadSubPackagePromise = new Promise(function (resolve, reject){
-    const loadTask = wx.loadSubpackage({
-      name: 'stage1', // name 可以填 name 或者 root
-      success: function (res) {
-        // 分包加载成功后通过 success 回调
-        resolve();
-      },
-      fail: function (res) {
-        reject();
-        // 分包加载失败通过 fail 回调
-      }
-    });
-    
-    loadTask.onProgressUpdate(res => {
-      subpackageLoadingInfo = res;
-      console.log('下载进度', res.progress)
-      console.log('已经下载的数据长度', res.totalBytesWritten)
-      console.log('预期需要下载的数据总长度', res.totalBytesExpectedToWrite)
-    });  
-  })
+  if (false) {
+    // 加载子包
+    const loadSubPackagePromise = new Promise(function (resolve, reject) {
+      const loadTask = wx.loadSubpackage({
+        name: 'stage1', // name 可以填 name 或者 root
+        success: function (res) {
+          // 分包加载成功后通过 success 回调
+          resolve();
+        },
+        fail: function (res) {
+          reject();
+          // 分包加载失败通过 fail 回调
+        }
+      });
 
-  // 等待子包的加载
-  loadSubPackagePromise.then(function(){
-    // 释放占用的资源
-    //gl.deleteProgram(programInfo.program);
-    //firstFlashTexture && gl.deleteTexture(firstFlashTexture);
-    programInfo = null;
-    buffers = null;
-    wx.setPreferredFramesPerSecond(60); // 恢复60帧,因为要开始游戏的内容渲染了    
-  });
-}
-  
+      loadTask.onProgressUpdate(res => {
+        subpackageLoadingInfo = res;
+        console.log('下载进度', res.progress)
+        console.log('已经下载的数据长度', res.totalBytesWritten)
+        console.log('预期需要下载的数据总长度', res.totalBytesExpectedToWrite)
+      });
+    })
+
+    // 等待子包的加载
+    loadSubPackagePromise.then(function () {
+      // 释放占用的资源
+      //gl.deleteProgram(programInfo.program);
+      //firstFlashTexture && gl.deleteTexture(firstFlashTexture);
+      programInfo = null;
+      buffers = null;
+      wx.setPreferredFramesPerSecond(60); // 恢复60帧,因为要开始游戏的内容渲染了    
+    });
+  }
+
   // 开始主循环
   requestAnimationFrame(loop);
   return true;
@@ -142,11 +118,40 @@ function _main_() {
 // ------------------------ 下面是封装的函数 ---------------------------------
 
 /**
+ * 初始化离屏纹理渲染的 shader
+ */
+function initOffScreenShaderProgram(gl) {
+  // 显示离屏背景纹理的 Vertex Shader
+  const vsSource = `
+    attribute vec2 aVertexPosition;
+    attribute vec2 aTextureCoord;
+    varying highp vec2 vTextureCoord;
+    void main(void){
+      gl_Position = vec4(aVertexPosition, 0.0, 1.0);
+      vTextureCoord = aTextureCoord;
+  }
+  `;
+
+  // 显示离屏纹理的 Fragment Shader
+  const fsSource = `
+    varying highp vec2 vTextureCoord;
+    uniform sampler2D uSampler;
+    void main(void){
+      gl_FragColor = texture2D(uSampler, vTextureCoord);
+    }
+  `;
+
+  // Initialize a shader program; this is where all the lighting
+  // for the vertices and so forth is established.
+  return initShaderProgram(gl, vsSource, fsSource);
+}
+
+/**
  * 按照 cocos 的方式来填充一些函数,具体见: stage1\libs\weapp-adapter\Canvas.js 文件与 stage1\libs\weapp-adapter\HTMLCanvasElement.js:26 文件的实现
  * 要不在真机上运行不过,因为下面这些函数会被使用
  * @param {*} canvas 
  */
-function initCanvas(canvas){
+function initCanvas(canvas) {
   canvas.type = 'canvas';
 
   canvas.getBoundingClientRect = function () {
@@ -256,40 +261,29 @@ function loadShader(gl, type, source) {
  * 初始化全屏矩形的 buffer
  * @param {Webgl} gl 
  */
-function initQuadBuffers(gl, bgWidth, bgHeight){
+function initFullScreenQuadBuffers(gl) {
   // Create a buffer for the rectangle's vertex positions.
   const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  // 距中显示背景,计算坐标
-  // 计算背景的缩放比
-  const widthScale = windowWidth/bgWidth;
-  const heightScale = windowHeight/bgHeight;
-  const bgScale = Math.max(widthScale, heightScale);
-  const width = bgWidth*bgScale;
-  const height = bgHeight*bgScale;
-  const x = width/windowWidth*2 - 1;  // 变换到投影坐标系
-  const y = height/windowHeight*2 - 1;// 变换到投影坐标系
-  
   // 创建全屏矩形的顶点坐标
   const positions = [
-    -x, -y,
-     x, -y,
-     x,  y,
-    -x,  y];
+    -1, -1,
+    1, -1,
+    1, 1,
+    -1, 1];
 
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
   // JavaScript array, then use it to fill the current buffer.
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-
   // 生成纹理坐标
   const textureCoordBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, textureCoordBuffer);
 
   const textureCoordinates = [
-    0.0, 1.0,    
+    0.0, 1.0,
     1.0, 1.0,
     1.0, 0.0,
     0.0, 0.0,
@@ -304,7 +298,7 @@ function initQuadBuffers(gl, bgWidth, bgHeight){
 
   // 顶点索引
   const indices = [
-    0, 1, 2,  0, 2, 3
+    0, 1, 2, 0, 2, 3
   ];
 
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
@@ -317,42 +311,6 @@ function initQuadBuffers(gl, bgWidth, bgHeight){
 }
 
 /**
- * 根据传入的 image 来创建对应的纹理
- */
-function loadTexture(gl, image){
-  const texture = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  return texture;
-}
-
-/**
- * 屏幕坐标()到投影坐标系
- */
-function screenToProjectCoord(screenX, screenY){
-  return [screenX/windowWidth*2.0 - 1.0, screenY/windowHeight*2.0 - 1.0];
-}
-
-/**
- * 加载指定的图片资源
- * @param {*} image 
- * @param {*} url 
- */
-function loadImage(url) {
-  return new Promise(function (resolve, _) {
-    const image = wx.createImage();
-    image.onload = function () {
-      resolve(image);
-    }
-
-    image.src = url;
-  });
-}
-
-/**
  * 绘制场景(首屏)
  * @param {*} gl 
  * @param {*} programInfo 
@@ -360,7 +318,7 @@ function loadImage(url) {
  * @param {*} texture 
  * @param {*} deltaTime 
  */
-function drawScene(gl, programInfo, buffers, texture, deltaTime){
+function drawScene(gl, programInfo, buffers, texture, deltaTime) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.depthFunc(gl.LEQUAL);
---
+++ wechatgame/offScreenCanvas.js
@@ -0,0 +1,104 @@
+/**
+ * 离屏的 Canvas,我们通过 canvas 绘制真正的首屏相关的内容,减少代码量
+ */
+
+const { windowWidth, windowHeight, pixelRatio} = wx.getSystemInfoSync();
+ let mCanvas;
+ let mCtx; 
+ let mTexture;          // 用于存储离屏渲染内容的纹理
+ let mWebGL;            // webGL
+ let mBgImage;          // 背景图片
+ let mLoadingBarImage;  // 进度条图片
+
+/**
+ * 初始化资源加载
+ * @param gl 使用的 webgl
+ * @returns 返回包含离屏的纹理
+ */
+function init(gl){    
+    // 创建离屏 Canvas
+    mCanvas = wx.createCanvas();    
+    mCanvas.width = windowWidth;//*pixelRatio;
+    mCanvas.height = windowHeight;//*pixelRatio;
+    mCtx = mCanvas.getContext('2d');      // 获取离屏的 canvas
+
+    mBgImage = wx.createImage();          // 背景图片
+    mLoadingBarImage = wx.createImage();  // 进度条背景图片
+    const bgImageLoading = loadImage(mBgImage, "first_package_images/first_flash.jpg");
+    const loadingBarImageLoading = loadImage(mLoadingBarImage, "first_package_images/loading_bar.png");
+
+    mWebGL = gl;
+    
+    return Promise.all([bgImageLoading, loadingBarImageLoading]).then(function(){
+        // 创建离屏使用的纹理
+        mTexture = gl.createTexture();
+        gl.bindTexture(mWebGL.TEXTURE_2D, mTexture);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+        // 绘制一帧
+        DrawScene();        
+        return mTexture;    // 返回当前使用的纹理
+    })
+}
+
+/**
+ * 绘制场景的函数
+ */
+function DrawScene (){
+    // 清除屏幕
+    mCtx.clearRect(0, 0, windowWidth, windowHeight)
+    
+    { // 距中显示背景
+        // 计算背景的缩放比
+        const widthScale = windowWidth/mBgImage.width;
+        const heightScale = windowHeight/mBgImage.height;
+        const bgScale = Math.max(widthScale, heightScale);
+        const width = mBgImage.width*bgScale;
+        const height = mBgImage.height*bgScale;
+        // 绘制背景
+        mCtx.drawImage(mBgImage, (windowWidth - width)*0.5, (windowHeight - height)*0.5, width, height);
+    }
+
+    mCtx.fillText("测试一下字体", 100, 100);
+
+    // 更新离屏纹理内容
+    mWebGL.bindTexture(mWebGL.TEXTURE_2D, mTexture);
+    mWebGL.texImage2D(mWebGL.TEXTURE_2D, 0, mWebGL.RGB, mWebGL.RGB, mWebGL.UNSIGNED_BYTE, mCanvas);
+    mWebGL.bindTexture(mWebGL.TEXTURE_2D, null);
+}
+
+/**
+ * 加载指定的图片资源
+ * @param {*} image 
+ * @param {*} url 
+ */
+function loadImage(image, url){
+    return new Promise(function(resolve, _){
+        image.onload = function(){
+        resolve(image);      
+        }
+
+        image.src = url;
+    });
+}
+
+/**
+ * 释放占用的资源
+ */
+function release(){
+    mWebGL.bindTexture(gl.TEXTURE_2D, null);
+    mWebGL.deleteTexture(mTexture);
+    mTexture = undefined;
+    mCtx = undefined;
+    mWebGL = undefined;    
+    mBgImage.src = "";
+    mBgImage = undefined;
+    mLoadingBarImage.src = "";
+    mLoadingBarImage = undefined;
+    mCanvas = undefined;
+}
+
+exports.init = init;
+exports.release = release;
